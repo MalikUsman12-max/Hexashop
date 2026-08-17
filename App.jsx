decd6d974f5892344387e8373f4925391b9efbda
MalikUsman12-max <iftikharusman679@gmail.com>
2026-08-16 20:08:45 -0700

    Add header and button to App component

--- App.jsx
+++ App.jsx
@@ -5,9 +5,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>Hexashop is working!</h1>
+
+      <button onClick={() => setCount(count + 1)}>
+        Count: {count}
+      </button>
+    </div>
   )
 }
 
